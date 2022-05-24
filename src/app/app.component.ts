8d44057669893c039e6576c5e0e09127ba0c803e
Brandon Vidal <brandonvidal93@gmail.com>
2022-05-24 16:35:00 -0500

    U - Modificación de menú

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,8 @@ import { MenuItem } from '@core/modelo/menu-item';
 export class AppComponent {
   title = 'app-base';
   public companies: MenuItem[] = [
-    { url: '/home', nombre: 'home' },
-    { url: '/producto', nombre: 'producto' }
+    { url: '/home', nombre: 'Inicio' },
+    { url: '/buildings', nombre: 'Inmuebles' }
     
   ];
 
